91671cbb1886f0e8c226fd2395e4eea67ee2a756
Bharati Garg <bharatigargkls1999@gmail.com>
2024-08-08 11:08:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Todo from "./component/Todo";
+
+const App = () => {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+};
+
+export default App;
